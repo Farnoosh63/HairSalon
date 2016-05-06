1b85e8ec2132ab3b6d763d3deee82d44bbdf400a
farnoosh <farnoosh@pdx.edu>
2016-05-06 15:16:26 -0700

    update the text in clients

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -17,8 +17,8 @@ public class Stylist {
   public int getId() {
     return id;
   }
-
-@Override
+  
+  @Override
   public boolean equals(Object otherStylist) {
     if(!(otherStylist instanceof Stylist)) {
       return false;
@@ -49,8 +49,8 @@ public class Stylist {
     try (Connection con = DB.sql2o.open()) {
       String sql = "SELECT * FROM stylists WHERE id=:id";
       Stylist stylist = con.createQuery(sql)
-        .addParameter("id", id)
-        .executeAndFetchFirst(Stylist.class);
+      .addParameter("id", id)
+      .executeAndFetchFirst(Stylist.class);
       return stylist;
     }
   }
@@ -59,11 +59,9 @@ public class Stylist {
     try(Connection con = DB.sql2o.open()) {
       String sql = "SELECT * FROM clients where stylistId=:id";
       return con.createQuery(sql)
-        .addParameter("id", this.id)
-        .executeAndFetch(Client.class);
+      .addParameter("id", this.id)
+      .executeAndFetch(Client.class);
     }
   }
-
-
 
 }
